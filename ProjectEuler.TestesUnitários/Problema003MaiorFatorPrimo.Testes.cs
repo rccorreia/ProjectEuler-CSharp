6ff5c67ea6a499851d904db1c3528138e6d6e7b6
Ricardo Correia <rdcc89@gmail.com>
2017-10-29 22:46:29 -0200

    Resolução do Problema 003 - Maior Fator Primo de um número

---
+++ ProjectEuler.TestesUnitários/Problema003MaiorFatorPrimo.Testes.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using ProjectEuler.Problema003MaiorFatorPrimo;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ProjectEuler.TestesUnitários
+{
+    public class Problema003MaiorFatorPrimo
+    {
+        [Fact]
+        public void DeveRetornarOMaiorFatorPrimosDe13195()
+        {
+            var resultadoMaiorFatorPrimo = 29;
+            var operaçõesMatemáticas = new OperaçõesMatemáticas();
+
+            var maiorFatorPrimoDe13195 = operaçõesMatemáticas.IdentificarMaiorFatorPrimosDoNúmero(13195);
+
+            maiorFatorPrimoDe13195.Should().Be(resultadoMaiorFatorPrimo);
+        }
+
+        [Fact]
+        public void DeveRetornarOsFatoresPrimosDe24()
+        {
+            var resultadoMaiorFatorPrimo = 3;
+            var operaçõesMatemáticas = new OperaçõesMatemáticas();
+
+            var maiorFatorPrimoDe13196 = operaçõesMatemáticas.IdentificarMaiorFatorPrimosDoNúmero(24);
+
+            maiorFatorPrimoDe13196.Should().Be(resultadoMaiorFatorPrimo);
+        }
+
+        [Fact]
+        public void DeveRetornarOMaiorFatorPrimoDoValor600851475143ComoResoluçãoDoProblema003()
+        {
+            var resultadoDeMaiorFatorPrimoDoNúmero600851475143 = 6857;
+            var operaçõesMatemáticas = new OperaçõesMatemáticas();
+
+            var maiorFatorPriomoDe600851475143 = operaçõesMatemáticas.IdentificarMaiorFatorPrimosDoNúmero(600851475143);
+
+            maiorFatorPriomoDe600851475143.Should().Be(resultadoDeMaiorFatorPrimoDoNúmero600851475143);
+        }
+    }
+}
---
+++ ProjectEuler/Problema003MaiorFatorPrimo/OperaçõesMatemáticas.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjectEuler.Problema003MaiorFatorPrimo
+{
+    public class OperaçõesMatemáticas
+    {
+        private long _númeroAuxiliar;
+
+        public long IdentificarMaiorFatorPrimosDoNúmero(long número)
+        {
+            if (número % 2 == 0)
+                return IdentificarMaiorFatorPrimosDoNúmeroPar(número);
+            else
+                return IdentificarMaiorFatorPrimosDoNúmeroÍmpar(número);
+        }
+
+        private long IdentificarMaiorFatorPrimosDoNúmeroPar(long númeroPar)
+        {
+            long maiorFatorPrimo;
+            _númeroAuxiliar = númeroPar / 2;
+
+            while (_númeroAuxiliar % 2 == 0)
+                _númeroAuxiliar = _númeroAuxiliar / 2;
+
+            maiorFatorPrimo = _númeroAuxiliar;
+
+            return maiorFatorPrimo;
+        }
+
+        private long IdentificarMaiorFatorPrimosDoNúmeroÍmpar(long númeroÍmpar)
+        {
+            long maiorFatorPrimo;
+            if (númeroÍmpar <= 1) return 0;
+            long _númeroAuxiliar = númeroÍmpar;
+
+            long fator = 3;
+            var fatorMáximo = Math.Sqrt(_númeroAuxiliar);
+            long últimoFator = 0;
+
+            while (_númeroAuxiliar > 1 && fator <= fatorMáximo)
+            {
+                if (_númeroAuxiliar % fator == 0)
+                {
+                    _númeroAuxiliar = _númeroAuxiliar / fator;
+                    últimoFator = fator;
+                    while (_númeroAuxiliar % fator == 0)
+                        _númeroAuxiliar = _númeroAuxiliar / fator;
+
+                    fatorMáximo = Math.Sqrt(_númeroAuxiliar);
+                }
+
+                fator += 2;
+            }
+
+            maiorFatorPrimo = _númeroAuxiliar;
+
+            return maiorFatorPrimo;
+        }
+    }
+}
